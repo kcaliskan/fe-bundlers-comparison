c682ff34318d5f985e189094176ac4c33ba52560
Kaan Caliskan <mail@kcaliskan.com>
2023-01-22 21:27:17 -0500

    vite react swc initial

---
+++ vite-react-swc/src/App.tsx
@@ -0,0 +1,47 @@
+import './App.css'
+import Car from './Car';
+import Car01 from './Car01';
+import Car02 from './Car02';
+import Car03 from './Car03';
+import Car04 from './Car04';
+import Car05 from './Car05';
+import Car06 from './Car06';
+import Car07 from './Car07';
+import Car08 from './Car08';
+import Car09 from './Car09';
+import Car10 from './Car10';
+import Car11 from './Car11';
+import Car12 from './Car12';
+import Car13 from './Car13';
+import Car14 from './Car14';
+
+// import Coin from './Coin';
+import Leaf from './Leaf';
+function App() {
+
+  return (
+    <div className="App">
+      <p>Hellozaff</p>
+      root: {Date.now()}
+      <Leaf />
+      <Car />
+      <Car01 />
+      <Car02 />
+      <Car03 />
+      <Car04 />
+      <Car05 />
+      <Car06 />
+      <Car07 />
+      <Car08 />
+      <Car09 />
+      <Car10 />
+      <Car11 />
+      <Car12 />
+      <Car13 />
+      <Car14 />
+      {/* <Coin /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ vite-react-swc/src/Leaf.tsx
@@ -0,0 +1,5 @@
+export default function Leaf() {
+  return <div>leaf: {Date.now()}
+  <p>hello leafzz</p>
+  </div>
+}
