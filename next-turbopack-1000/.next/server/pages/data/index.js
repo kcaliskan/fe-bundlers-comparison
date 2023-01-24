098e8b9a1e766b41094743431d188479a4437206
Kaan Caliskan <mail@kcaliskan.com>
2023-01-24 03:39:36 -0500

    up

--- next-turbopack-1000/.next/server/pages/data/index.js
+++
@@ -1,7 +0,0 @@
-Error.stackTraceLimit = 100;
-global.self = global;
-require("./chunks/_f6b35b.js");
-require("./chunks/pages_index.tsx.js");
-require("./chunks/[embedded_modules]_@vercel_turbopack-next_internal_page-server-handler.tsx.js");
-require("./chunks/[embedded_modules]_@vercel_turbopack-next_ipc_index.ts.js");
-require("./chunks/[embedded_modules]_@vercel_turbopack-next_compiled_stacktrace-parser_index.js");
